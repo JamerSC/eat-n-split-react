11dab91441aa1d6ebe6e3f282b33b97da12c7edf
Jamer Catalla <mr.catalla28@gmail.com>
2025-03-31 08:01:28 +0800

    Prepared Friend List Components & Display

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,48 @@
-function App() {
-  return <div className="App">Eat N Split</div>;
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <FriendList />
+    </div>
+  );
 }
 
-export default App;
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <list>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.name} />
+      ))}
+    </list>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      {friend.name}
+    </li>
+  );
+}
